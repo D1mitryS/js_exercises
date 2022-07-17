59ff7ced61dd87eccb00bb3ec9ef936155cf8ece
D1mitryS <D1mitryS@pm.me>
2022-07-17 17:04:09 +0300

    update solution for CapitalizeEveryNSymbol.js

--- CapitalizeEveryNSymbol.js
+++ CapitalizeEveryNSymbol.js
@@ -11,5 +11,10 @@ const capitalizeEveryNSymbol = (text, symbolNum) => {
 
 const text = 'few words inside a string';
 console.log(capitalizeEveryNSymbol(text, 3));
+console.assert(capitalizeEveryNSymbol(text, 3) === 'feW wOrdS iNsiDe A sTriNg');
+
 console.log(capitalizeEveryNSymbol(text, 2));
+console.assert(capitalizeEveryNSymbol(text, 2) === 'fEw wOrDs iNsIdE A StRiNg');
+
 console.log(capitalizeEveryNSymbol(text, 1));
+console.assert(capitalizeEveryNSymbol(text, 1) === 'FEW WORDS INSIDE A STRING')
