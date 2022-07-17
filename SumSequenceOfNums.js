828615c35f4f55106e56fea1d4e728acb5dae1f5
D1mitryS <D1mitryS@pm.me>
2022-07-17 18:41:52 +0300

    update solution for sumSequenceofNums.js

--- SumSequenceOfNums.js
+++ SumSequenceOfNums.js
@@ -4,16 +4,15 @@ The sequence is given by two values: begin - the beginning of the sequence,
 end - the end of the sequence
 */
 const sumSequence = (begin, end) => {
-    if (begin > end) {
-        return NaN;
-    }
-    else if (begin === end) {
-        console.log(`sumSequence(${begin}) = ${begin}`)
-        return begin;
-    };
-    
-    console.log(`sumSequence(${begin}) = ${begin} + sumSequence(${begin + 1})`)
+    if (begin > end) return NaN;
+    else if (begin === end) return begin;
+
     return begin + sumSequence(begin + 1, end);
 };
 
-console.log(sumSequence(5, 9))
+console.log(sumSequence(5, 9));
+console.assert(sumSequence(5, 9) === 35);
+
+console.log('');
+console.log(sumSequence(3, 7));
+console.assert(sumSequence(3, 7) === 25)
