0c41bf4d0c14a64cc3189a694a690285939135dd
D1mitryS <D1mitryS@pm.me>
2022-07-17 16:54:49 +0300

    update solution for FindFactorial.js

--- FindFactorial.js
+++ FindFactorial.js
@@ -1,30 +1,38 @@
-//Get factorial n using recursion
-const getFactorialUsingRecursion = n => {
+//Get factorial n using recursive process
+const factorialRec = n => {
     if (n === 1) return 1;
-    return n * getFactorialUsingRecursion(n - 1);
+
+    return n * factorialRec(n - 1);
 };
 
 console.log('recursion:');
-console.log(`2! = ${getFactorialUsingRecursion(2)}`);
-console.log(`4! = ${getFactorialUsingRecursion(4)}`);
+console.log(`2! = ${factorialRec(2)}`);
+console.assert(factorialRec(2) === 2 , `2! != 2`);
+
+console.log(`4! = ${factorialRec(4)}`);
+console.assert(factorialRec(4) === 24, `4! != 24`)
 
 
-//Get factorial n using iteration
-const getFactorialUsingIteration = n => {
-    const iter = (counter, acc) => {
-        if (counter <= 1) return acc;
-        return iter(counter - 1, acc * counter)
+//Get factorial n using iterative process
+const factorialIter = n => {
+    const iter = (counter, result) => {
+        if (counter <= 1) return result;
+
+        return iter(counter - 1, result * counter)
     };
     return iter(n, 1);
 };
 
 console.log('\niteration:');
-console.log(`!1 = ${getFactorialUsingIteration(1)}`);
-console.log(`!3 = ${getFactorialUsingIteration(3)}`)
+console.log(`1! = ${factorialIter(1)}`);
+console.assert(factorialIter(1) === 1, `1! != 1`);
+
+console.log(`3! = ${factorialIter(3)}`);
+console.assert(factorialIter(3) === 6, `3! != 6`);
 
 
-//Get factorial n using "for" loop
-const getFactorialUsingLoop = (n) => {
+//Get factorial n using a loop
+const factorialLoop = (n) => {
     let result = 1;
 
     for (let i = 1; i <= n; i++) {
@@ -34,5 +42,8 @@ const getFactorialUsingLoop = (n) => {
 };
 
 console.log('\nloop:');
-console.log(`5! = ${getFactorialUsingLoop(5)}`);
-console.log(`0! = ${getFactorialUsingLoop(0)}`);
+console.log(`5! = ${factorialLoop(5)}`);
+console.assert(factorialLoop(5) === 120, `5! != 120`);
+
+console.log(`0! = ${factorialLoop(0)}`);
+console.assert(factorialLoop(0) === 1, `0! != 1`);
