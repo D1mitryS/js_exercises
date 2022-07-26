f2e8b0dcabc0990a309b2edf7ab65d3b6fefc2f3
D1mitryS <D1mitryS@pm.me>
2022-07-26 23:10:58 +0300

    update solution of CapitalizeEveryNsymbol/recursive.js

--- CapitalizeEveryNSymbol/recursive.js
+++ CapitalizeEveryNSymbol/recursive.js
@@ -1,7 +1,7 @@
 export const capitalizeRecursively = (str, n, i = 0) => {
-    if ((i === str.length - 1) && ((i + 1) % n === 0)) return str[i].toUpperCase();
-    else if (i === str.length - 1) return str[i];
+    const shouldBeBig = (i + 1) % n === 0;
+    if (i === str.length - 1) return (shouldBeBig) ? str[i].toUpperCase() : str[i];
   
-    else if ((i + 1) % n === 0) return str[i].toUpperCase() + capitalizeRecursively(str, n, i + 1);
-    return str[i] + capitalizeRecursively(str, n, i + 1);
+    return (shouldBeBig) ? str[i].toUpperCase() + capitalizeRecursively(str, n , i + 1) 
+    : str[i] + capitalizeRecursively(str, n, i + 1);
   };
